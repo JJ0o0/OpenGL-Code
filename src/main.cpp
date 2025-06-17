6873b3a25cedb9b3fa5593814334aea713466390
JJ0o0 <87098275+JJ0o0@users.noreply.github.com>
2025-06-17 08:28:08 -0300

    Adicionado comentários para facilitar leitura.

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,14 @@
 #include "window/window.h"
 
 int main() {
-    Window wnd(800, 600, "J0o0ll Janela");
-    wnd.Init();
+    Window wnd(800, 600, "J0o0ll Janela"); // Criando a janela.
+    wnd.Init(); // Inicializando.
 
+    // Caso a janela não deve fechar.
     while (!wnd.ShouldClose()) {
-        wnd.Update();
+        wnd.Update(); // Atualizar a janela.
     }
 
+    // Aqui, é chamado o deconstrutor da janela e fechado o programa com sucesso.
     return 0;
 }
--- src/window/window.cpp
+++ src/window/window.cpp
@@ -3,67 +3,84 @@
 #include <GLFW/glfw3.h>
 #include <iostream>
 
+// Define variáveis necessárias para a janela.
 Window::Window(int width, int height, const char* title) 
     : window_width(width), window_height(height), window_title(title) {}
 
+// Destroi, se houver, o objeto da janela e desliga o GLFW.
 Window::~Window() {
     if (window) glfwDestroyWindow(window);
     
     glfwTerminate();
 }
 
+// Inicializa a janela GLFW e o GLAD.
 void Window::Init() {
+    // Inicializando GLFW, e definindo hints.
     glfwInit();
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);                  // Versão máxima do OpenGL.
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);                  // Versão mínima do OpenGL
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);  // Profile do OpenGL
+    glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);                     // Janela aumentável.
 
+    // Criando a janela.
+    // Aqui passo as variáveis que defini no construtor (window_width, window_height, window_title).
     window = glfwCreateWindow(
         window_width, window_height, 
         window_title, 
         NULL, NULL
     );
 
+    // Caso a janela não conseguir ser criada com sucesso, ele irá dar um erro no terminal e encerrar o programa.
+    // Assim, ele vai logo para o deconstrutor.
     if (window == NULL) {
         std::cerr << "A criação da janela GLFW falhou!\n";
 
-        window = nullptr;
+        window = nullptr; // Transformando window em null pointer para liberação de memória.
         glfwTerminate();
 
         return;
     }
 
+    // Faz com que a janela seja o contexto atual.
     glfwMakeContextCurrent(window);
+
+    // Definindo callback de caso a janela for mudada de tamanho (por algum mótivo).
     glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
+    // Inicializando o GLAD. Se não for possível inicializar, acaba retornando um erro no terminal.
     if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
         std::cerr << "Não foi possível inicializar o GLAD!\n";
     }
 }
 
+// Método que roda a cada frame no Game Loop.
 void Window::Update() {
-    // INPUT
+    // Input
     InputHandling();
 
-    // RENDERIZAR
+    // Renderização
     glClearColor(.2f, .3f, .3f, 1.0f);
     glClear(GL_COLOR_BUFFER_BIT);
 
-    // EVENTOS E BUFFERS
+    // Eventos e Buffers
     glfwSwapBuffers(window);
     glfwPollEvents();
 }
 
+// Detecção de Input (sem ser via callbacks).
 void Window::InputHandling() {
     if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
         glfwSetWindowShouldClose(window, true);
     }
 }
 
+// Detecção para caso a janela deve fechar.
 bool Window::ShouldClose() {
     return glfwWindowShouldClose(window);
 }
 
+// Método que define tamanho do viewport para o tamanho da janela.
 void Window::framebuffer_size_callback(GLFWwindow* window, int width, int height) {
     glViewport(0, 0, width, height);
 }
